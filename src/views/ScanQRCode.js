3bcb76585c63a34e902eaf4df5b3d7f0e36a6957
amadrzyk <alexmadrzyk@gmail.com>
2018-11-28 21:51:57 -0500

    Testing something

--- src/views/ScanQRCode.js
+++ src/views/ScanQRCode.js
@@ -1,9 +1,16 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Alert } from "react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
 
 import { BackHeader } from "../components/Headers";
 import { QRCamera } from "../components/Camera";
 import { FullWidthImage } from "../components/Images";
+
+const styles = EStyleSheet.create({
+  cameraView: {
+    backgroundColor: "$primaryGray"
+  }
+});
 
 export default class ScanQRCode extends React.Component {
   constructor(props) {
@@ -13,24 +20,32 @@ export default class ScanQRCode extends React.Component {
   }
 
   handleScan(data) {
+    this.handleError("test");
+
     if (data) {
       let searchString = ".com";
       let path = data.substring(
         data.indexOf(searchString) + searchString.length
       );
-      this.props.history.push(path);
+      // this.props.history.push(path);
     }
   }
 
   handleError(err) {
     console.error(err);
+    Alert.alert(
+      "An error occurred :(",
+      err,
+      [{ text: "OK", onPress: () => console.log("OK Pressed") }],
+      { cancelable: true }
+    );
   }
 
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: "#01a7b7" }}>
         <BackHeader />
-        <View>
+        <View style={styles.cameraView}>
           <QRCamera
             handleScan={this.handleScan}
             handleError={this.handleError}
@@ -40,7 +55,6 @@ export default class ScanQRCode extends React.Component {
         <View
           style={{
             flex: 1,
-            // justifyContent: "flex-end",
             zIndex: -1
           }}
         >
