0efd6a760740ffb12f14bfeacd7522ad7330fbe3
Nik Yerasi <nyerasi@yahoo.com>
2019-03-21 13:49:18 -0700

    set up skeleton for search results

---
+++ build/precache-manifest.35dcdb5deb8240d290d95283bcaf6984.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "82e5c6d07462f446950688b35c768992",
+    "url": "/static/media/bampfa_logo.82e5c6d0.png"
+  },
+  {
+    "revision": "324bce08c5af3f7bee05",
+    "url": "/static/css/main.aadbf83b.chunk.css"
+  },
+  {
+    "revision": "d13ccd28b18476ee11de50061e5440b3",
+    "url": "/static/media/Tutorial.d13ccd28.png"
+  },
+  {
+    "revision": "bd938336d87ba97f805b",
+    "url": "/static/js/1.bd938336.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "57b5ac6b38d427ee6ad7278126b0418f",
+    "url": "/static/media/loading.57b5ac6b.gif"
+  },
+  {
+    "revision": "324bce08c5af3f7bee05",
+    "url": "/static/js/main.324bce08.chunk.js"
+  },
+  {
+    "revision": "d6e27409248f4150907fc6c678f5e17a",
+    "url": "/static/media/IndianSunsetBlur.d6e27409.jpg"
+  },
+  {
+    "revision": "659ead3560cd9bf2ed5cfa6ee416175c",
+    "url": "/static/media/essay_title_white.659ead35.png"
+  },
+  {
+    "revision": "d4ea8c31ccb21eb6d08528c15dbc6d84",
+    "url": "/static/media/HansHofmann.d4ea8c31.png"
+  },
+  {
+    "revision": "bd938336d87ba97f805b",
+    "url": "/static/css/1.1f66fb9a.chunk.css"
+  },
+  {
+    "revision": "26d202c9c5543f0b6a882d62099b8cad",
+    "url": "/index.html"
+  }
+];
